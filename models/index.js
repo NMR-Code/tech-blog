1be40146af8cec68440181a75bc5ccddd50971cb
NMR <77173929+NMR-Code@users.noreply.github.com>
2021-06-29 13:58:55 -0400

    updating models for posts, comments, and users

--- models/index.js
+++ models/index.js
@@ -1,13 +1,31 @@
-const User = require('./User');
-const Project = require('./Project');
+// This is where we will set up relationships for the models
+// Import all the models
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
 
-User.hasMany(Project, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+//set up relationships
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+//associations for the commments
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
 });
 
-Project.belongsTo(User, {
-  foreignKey: 'user_id'
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
 });
 
-module.exports = { User, Project };
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+
+module.exports = { User, Post, Comment };

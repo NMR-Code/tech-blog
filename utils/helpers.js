d5fb7411c59b7c744433d173addc6a09d6dcea5c
NMR <77173929+NMR-Code@users.noreply.github.com>
2021-06-29 14:06:20 -0400

    implemented the handlebars templates for the posts as well as the general page layout

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,22 +1,5 @@
-module.exports = {
-  format_date: (date) => {
-    // Format date as MM/DD/YYYY
-    return date.toLocaleDateString();
-  },
-  format_amount: (amount) => {
-    // format large numbers with commas
-    return parseInt(amount).toLocaleString();
-  },
-  get_emoji: () => {
-    const randomNum = Math.random();
+function compareStrings(str1, str2) {
+  return str1.equals(str2);
+}
 
-    // Return a random emoji
-    if (randomNum > 0.7) {
-      return `<span for="img" aria-label="lightbulb">💡</span>`;
-    } else if (randomNum > 0.4) {
-      return `<span for="img" aria-label="laptop">💻</span>`;
-    } else {
-      return `<span for="img" aria-label="gear">⚙️</span>`;
-    }
-  },
-};
+module.exports = compareStrings;
